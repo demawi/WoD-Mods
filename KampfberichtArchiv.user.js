9767b98f21dc8a1cf370819dab0f5d517b35be9b
demawi <demawi@gmail.com>
2025-05-29 16:23:55 +0200

    Weiterentwicklung

--- KampfberichtArchiv.user.js
+++ KampfberichtArchiv.user.js
@@ -50,12 +50,11 @@
         // Für den DB-Proxy muss man im Firefox openWindow statt nem IFrame benutzen
 
         static async startMod() {
-            if (demawiRepository.ensureIframeWrap()) return;
             // Seite der Main-Domain, wird ggf. als Cross-Site-Proxy verwendet
-            if (window.location.hostname.match(/^world-of-dungeons\.de$/)) {
-                if (window.location.href.endsWith("messenger=true")) {
+            if (!window.location.hostname.match(/^(.*)\.world-of-dungeons\.de$/)) {
+                if (window.location.href.includes("messengerId=")) {
                     await MyStorage.initMyStorage(false, "Main");
-                    _CSProxy.actAsCSProxyResponder();
+                    _CSProxy.actAsCrossSiteProxy(async (code) => eval(code)); // and eval here
                 }
                 return;
             } else {
