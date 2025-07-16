3b321570fc96f50d9d5a8ab3ca9ffb3076c88cac
demawi <demawi@gmail.com>
2025-07-16 23:26:03 +0200

    Versionsupdate

--- personal/ItemDatenbank.user.js
+++ personal/ItemDatenbank.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           [WoD] Item-Datenbank
-// @version        0.12.0
+// @version        0.12.1
 // @author         demawi
 // @namespace      demawi
 // @description    Datenbank der Items und Suche
