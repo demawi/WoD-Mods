30bd185497a60e478cf6918a1a0af64054e3152d
demawi <demawi@gmail.com>
2025-07-23 12:10:16 +0200

    Weiterentwicklung

--- AusruesterPlus.user.js
+++ AusruesterPlus.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           [WoD] Ausrüster Plus
-// @version        0.8.7
+// @version        0.8.8
 // @author         demawi
 // @namespace      demawi
 // @description    Erweiterungen für die Ausrüstung.
