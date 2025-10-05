b5f21fdf3876047993b443444178e6bbe83da9dc
demawi <demawi@gmail.com>
2025-10-06 01:06:10 +0200

    Weiterentwicklung

--- KampfkonfigPlus.user.js
+++ KampfkonfigPlus.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           [WoD] Kampfkonfig Plus
-// @version        1.1.7
+// @version        1.1.8
 // @author         demawi
 // @namespace      demawi
 // @description    Erweiterungen für die Kampfkonfigs. Aktuell nur JSON-Export/Import.
